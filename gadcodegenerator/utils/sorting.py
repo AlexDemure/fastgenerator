a4657f644d1f52e36c6b7852ab4e4bccfe10a05e
Alexander Grishchenko <alexanderdemure@gmail.com>
2025-04-12 23:16:12 +0500

    re

--- gadcodegenerator/utils/sorting.py
+++ gadcodegenerator/utils/sorting.py
@@ -6,5 +6,5 @@ from gadcodegenerator import const
 def sortimports(lines: list[str]) -> str:
     imports, code = [], []
     for line in lines:
-        (imports if re.match(const.REGEXP_IMPORT_PATTERN, line) else code).append(line)
+        (imports if re.match(const.REGEXP_IMPORT, line) else code).append(line)
     return const.SYMBOL_EMPTY.join(sorted(imports) + code)
